c4f5bfdb0e6569c4a18fb68ca17e395e2f7d55ed
unknown <trung.phankimthanh@gmail.com>
2024-08-04 10:38:39 +0700

    Initial commit for Deploy_to_VMSS branch

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -31,14 +31,14 @@ config_integration.trace_integrations(["requests"])
 # Logging
 logger = logging.getLogger(__name__)
 handler = AzureLogHandler(
-    connection_string="InstrumentationKey=56dcb607-e409-4e63-b81a-f85a57629a02"
+    connection_string="InstrumentationKey=7ee03697-b427-4db2-8cc8-8c9ee1ff2e47"
 )
 handler.setFormatter(logging.Formatter("%(traceId)s %(spanId)s %(message)s"))
 logger.addHandler(handler)
 # Logging custom Events
 logger.addHandler(
     AzureEventHandler(
-        connection_string="InstrumentationKey=56dcb607-e409-4e63-b81a-f85a57629a02"
+        connection_string="InstrumentationKey=7ee03697-b427-4db2-8cc8-8c9ee1ff2e47"
     )
 )
 # Set the logging level
@@ -47,14 +47,14 @@ logger.setLevel(logging.INFO)
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
     enable_standard_metrics=True,
-    connection_string="InstrumentationKey=56dcb607-e409-4e63-b81a-f85a57629a02",
+    connection_string="InstrumentationKey=7ee03697-b427-4db2-8cc8-8c9ee1ff2e47",
 )
 view_manager.register_exporter(exporter)
 
 # Tracing
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string="InstrumentationKey=56dcb607-e409-4e63-b81a-f85a57629a02"
+        connection_string="InstrumentationKey=7ee03697-b427-4db2-8cc8-8c9ee1ff2e47"
     ),
     sampler=ProbabilitySampler(1.0),
 )
@@ -65,7 +65,7 @@ app = Flask(__name__)
 middleware = FlaskMiddleware(
     app,
     exporter=AzureExporter(
-        connection_string="InstrumentationKey=56dcb607-e409-4e63-b81a-f85a57629a02"
+        connection_string="InstrumentationKey=7ee03697-b427-4db2-8cc8-8c9ee1ff2e47"
     ),
     sampler=ProbabilitySampler(rate=1.0),
 )
